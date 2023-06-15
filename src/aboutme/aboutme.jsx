29f0edf97c6f5380c648d8637aa85b632fd5e55f
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-06-14 21:23:48 -0700

    Update aboutme.jsx

--- src/aboutme/aboutme.jsx
+++ src/aboutme/aboutme.jsx
@@ -9,8 +9,8 @@ const Aboutme = () => {
       <div  class="w-[45%] ">        
         <img class="rounded-lg" src="https://rare-gallery.com/thumbs/5365780-work-computer-phone-code-book-plan-keys-table-lens-topdown-pen-paper-desk-office-floor-developer-dev-len-laptop-wood-creative-commons-images.jpg" alt="" srcset="" />
 </div>
-      <div class="flex-1 pr-[3rem] " >
-        <p><FaQuoteLeft/> <span className="text-[red] text-[2rem]">Hi am Bethwel,</span><br/> a passionate MERN stack developer with a strong foundation in JavaScript and experience in building web applications. I specialize in creating responsive and interactive user interfaces using React.js and integrating them with backend APIs built with Node.js and Express.js.
+      <div class="flex-1 pr-[3rem] bg-[#F9F5F6] pl-[25px] pt-2 pb-4 rounded-lg" >
+        <p><FaQuoteLeft/> <span className="text-[red] text-[2rem]">Hi am Bethwel,</span><br/> a passionate FULL stack developer with a strong foundation in JavaScript and experience in building web applications. I specialize in creating responsive and interactive user interfaces using React.js and integrating them with backend APIs built with Node.js and Express.js.
 
 
 With a keen eye for detail and a commitment to writing clean, maintainable code, I strive to create high-quality software that not only meets the client's requirements but also adheres to industry best practices. I am constantly seeking opportunities to learn and grow, as I believe that continuous improvement is key to staying ahead in the fast-paced world of web development.
