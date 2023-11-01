6fbbb0553040710b6627d7bae00dfd793c1192dd
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-11-01 22:37:05 +0300

    Update navbar.jsx

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
   return (
+
+    
      <div class="flex justify-between mt-2 mr-10 ml-[8rem]">
           <div>
           <a class="font-extrabold cursor-pointer text-[19px] hover:text-[red] " href='/'>Bethwel-dev</a>
       
     </div>
-
-          
           <div >
      <ul class="">
               <a class="hover:text-[red]  mr-[39px]" href="https://docdro.id/kQEg2XE">Download CV </a> 
